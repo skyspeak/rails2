bb83974636edd66e063fe367c8e5cc09be510a9e
Meghbartma Gautam <skyspeak@gmail.com>
2014-03-23 23:01:27 -0700

    new folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aqw1::Application.config.secret_key_base = '285065e4ea5beaa7ab9309ee6594206ee2c05ab706a73623a9f32435e1f50944808697dc74be427f52e5f3dd6e5123d5797ab87be4b7e36b213dc93478c59d6d'
